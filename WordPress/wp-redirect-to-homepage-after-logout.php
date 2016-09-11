2c74cc9814fee78d9b0b978a5f69d57df7878572
A7madev <A7maDev@Gmail.com>
2016-09-11 10:56:39 +0300

    Update wp-redirect-to-homepage-after-logout.php

--- WordPress/wp-redirect-to-homepage-after-logout.php
+++ WordPress/wp-redirect-to-homepage-after-logout.php
@@ -1,5 +1,7 @@
 <?php
 
+// functions.php
+// Redirect to homepage after user logout
 add_action('wp_logout','wppbc_redirect_logout');
 function wppbc_redirect_logout(){
     wp_redirect( home_url() );
